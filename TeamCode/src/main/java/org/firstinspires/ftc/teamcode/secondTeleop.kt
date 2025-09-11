7c2b68cfb739762ec3ded5a3399a548ca5c2fd12
AJ <ajjazzy1113@gmail.com>
2025-09-10 16:19:30 -0800

    Update secondTeleop.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode
 
-class secondTeleop {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+
+class secondTeleop: LinearOpMode() {
+
+    override fun runOpMode() {
+        val frontLeft = hardwareMap.get("frontLeft") as DcMotor
+        val frontRight = hardwareMap.get("frontRight") as DcMotor
+        val backLeft = hardwareMap.get("backLeft") as DcMotor
+        val backRight = hardwareMap.get("backRight") as DcMotor
+    }
 }
