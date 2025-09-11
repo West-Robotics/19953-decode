2aa671af5716cfd2f430dd920766c69a6753533f
sanhyune <fiddleheadreka501@gmail.com>
2025-09-10 16:05:52 -0800

    Update firstTeleop.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstTeleop.kt
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode
 
-class firstTeleop {
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+class firstTeleop: LinearOpMode(){
+
+    override fun runOpMode(){
+        val frontLeft = hardwareMap.get("frontLeft")
+        val frontRight = hardwareMap.get("frontRight")
+        val backLeft = hardwareMap.get("backLeft")
+        val backRight = hardwareMap.get("backRight")
+    }
 }
