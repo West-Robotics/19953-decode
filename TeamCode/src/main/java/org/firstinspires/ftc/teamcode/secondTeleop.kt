4afa90efa88192906662cb8e74ee32fab0341fdb
AJ <ajjazzy1113@gmail.com>
2025-09-12 16:01:02 -0800

    Update secondTeleop.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
@@ -1,14 +1,59 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 
-class secondTeleop: LinearOpMode() {
+@TeleOp(Name="2ndTeleOp")
 
-    override fun runOpMode() {
+class firstTeleop: LinearOpMode(){
+
+    override fun runOpMode(){
         val frontLeft = hardwareMap.get("frontLeft") as DcMotor
         val frontRight = hardwareMap.get("frontRight") as DcMotor
         val backLeft = hardwareMap.get("backLeft") as DcMotor
         val backRight = hardwareMap.get("backRight") as DcMotor
+
+        waitForStart()
+
+        while(opModeIsActive()){
+            if (gamepad1.a){
+                frontLeft.power = 1.0
+                frontRight.power = 1.0
+                backLeft.power = 1.0
+                backRight.power = 1.0
+            } else {
+                frontLeft.power = 0.0
+                frontRight.power = 0.0
+                backLeft.power = 0.0
+                backRight.power = 0.0
+            }
+
+            if (gamepad1.x){
+                frontRight.power = 1.0
+            } else {
+                frontRight.power = 0.0
+            }
+
+            if (gamepad1.b){
+                backLeft.power = 1.0
+            } else {
+                backLeft.power = 0.0
+            }
+
+            if (gamepad1.y){
+                frontLeft.power = 1.0
+                frontRight.power = 1.0
+                backLeft.power = 1.0
+                backRight.power = 1.0
+            } else {
+                frontLeft.power = 0.0
+                frontRight.power = 0.0
+                backLeft.power = 0.0
+                backRight.power = 0.0
+            }
+
+        }
+
     }
 }
