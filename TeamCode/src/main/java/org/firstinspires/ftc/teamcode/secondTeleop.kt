0027e40097cb1e13c371568f0bf6205d79dd42c4
That1Neet <nmarshall907@gmail.com>
2025-10-21 15:23:20 -0800

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 
 
 @TeleOp(name="PushBot")
@@ -15,8 +16,23 @@ class SecondTeleop: LinearOpMode() {
         val backLeft = hardwareMap.get("backLeft") as DcMotor
         val backRight = hardwareMap.get("backRight") as DcMotor
 
-        waitForStart()
+        backRight.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        backLeft.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        frontRight.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        frontLeft.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
 
+        backRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        backLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        frontRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        frontLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+
+        backRight.direction = DcMotorSimple.Direction.REVERSE
+        backLeft.direction = DcMotorSimple.Direction.FORWARD
+        frontRight.direction = DcMotorSimple.Direction.REVERSE
+        frontLeft.direction = DcMotorSimple.Direction.FORWARD
+
+        waitForStart()
 
         while (opModeIsActive()) {
 
@@ -25,14 +41,14 @@ class SecondTeleop: LinearOpMode() {
             val rx = gamepad1.right_stick_x.toDouble()
             val y = -gamepad1.left_stick_y.toDouble()
 
-            while (opModeIsActive()) {
-                frontRight.power = y - x + rx
-                frontLeft.power = y + x - rx
-                backLeft.power = y - x - rx
-                backRight.power = y + x + rx
+
+            frontRight.power = y - x + rx
+            frontLeft.power = y + x - rx
+            backLeft.power = y - x - rx
+            backRight.power = y + x + rx
 
 
-            }
+
 
         }
     }
