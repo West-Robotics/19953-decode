35ce30c8ce2006572c53f7ec54e6e6f7d43fc072
AJ <ajjazzy1113@gmail.com>
2025-10-21 16:20:51 -0800

    Update firstTeleop.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstTeleop.kt
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 
 
-@TeleOp(name="hehe")
+@TeleOp(name="ActualBot")
 class FirstTeleop: LinearOpMode() {
 
 
