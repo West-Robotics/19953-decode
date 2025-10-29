dfd9c38283755e7d083ae3d4e408ebeb7a6fa2d5
AJ <ajjazzy1113@gmail.com>
2025-10-28 23:53:30 -0800

    Update secondTeleop.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/secondTeleop.kt
@@ -2,6 +2,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.Gamepad
+
+
 
 
 @TeleOp(name="ActualBot")
@@ -9,37 +12,45 @@ class SecondTeleop: LinearOpMode() {
 
 
         override fun runOpMode() {
-                /*val frontLeft = hardwareMap.get("frontLeft") as DcMotor
+
+            val currentGamepad1 = Gamepad()
+            val currentGamepad2 = Gamepad()
+
+            val previousGamepad1 = Gamepad()
+            val previousGamepad2 = Gamepad()
+
+
+                val frontLeft = hardwareMap.get("frontLeft") as DcMotor
                 val frontRight = hardwareMap.get("frontRight") as DcMotor
                 val backLeft = hardwareMap.get("backLeft") as DcMotor
-                val backRight = hardwareMap.get("backRight") as DcMotor*/
+                val backRight = hardwareMap.get("backRight") as DcMotor
                 val flyWheel0 = hardwareMap.get("flyWheel0") as DcMotor
                 val flyWheel1 = hardwareMap.get("flyWheel1") as DcMotor
                 val throngler = hardwareMap.get("throngler") as DcMotor
                 var togglethrongler = false
-                var toggleflywheel = false
+                var toggleflywheels = false
 
-                /*backRight.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+                backRight.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
                 backLeft.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
                 frontRight.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-                frontLeft.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER*/
+                frontLeft.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
                 flyWheel0.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
                 flyWheel1.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
                 throngler.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
 
-                /*backRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+                backRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
                 backLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
                 frontRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-                frontLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE*/
+                frontLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
                 flyWheel0.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
                 flyWheel1.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
                 throngler.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
 
 
-                /*backRight.direction = DcMotorSimple.Direction.FORWARD
+                backRight.direction = DcMotorSimple.Direction.FORWARD
                 backLeft.direction = DcMotorSimple.Direction.REVERSE
                 frontRight.direction = DcMotorSimple.Direction.REVERSE
-                frontLeft.direction = DcMotorSimple.Direction.FORWARD*/
+                frontLeft.direction = DcMotorSimple.Direction.FORWARD
                 flyWheel0.direction = DcMotorSimple.Direction.FORWARD
                 flyWheel1.direction = DcMotorSimple.Direction.REVERSE
                 throngler.direction = DcMotorSimple.Direction.FORWARD
@@ -53,31 +64,39 @@ class SecondTeleop: LinearOpMode() {
                 while (opModeIsActive()) {
 
 
-                    /*val x = gamepad1.left_stick_x.toDouble() * 1.1
+                    previousGamepad1.copy(currentGamepad1)
+                    previousGamepad2.copy(currentGamepad2)
+
+                    currentGamepad1.copy(gamepad1)
+                    currentGamepad2.copy(gamepad2)
+
+                    val x = gamepad1.left_stick_x.toDouble() * 1.1
                     val rx = gamepad1.right_stick_x.toDouble()
-                    val y = -gamepad1.left_stick_y.toDouble()*/
+                    val y = -gamepad1.left_stick_y.toDouble()
                     val v = gamepad1.left_bumper
+                    val vv = previousGamepad1.left_bumper
                     val t = gamepad1.right_bumper
+                    val tt = previousGamepad1.right_bumper
 
 
-                    if (t && !t) {
+                    if (t && !tt) {
 
-                        togglethrongler = !togglethrongler
+                       togglethrongler = !togglethrongler
                     }
 
-                    if (v && !v) {
+                    if (v && !vv) {
 
-                        toggleflywheel = !toggleflywheel
+                        toggleflywheels = !toggleflywheels
                     }
 
 
-                    /*frontRight.power =  y - x + rx
+                    frontRight.power =  y - x + rx
                     frontLeft.power =  y + x - rx
                     backLeft.power =  y - x - rx
-                    backRight.power =  y + x + rx*/
+                    backRight.power =  y + x + rx
 
 
-                    if (toggleflywheel) {
+                    if (toggleflywheels) {
                         flyWheel0.power = 1.toDouble()
                         flyWheel1.power = (-1).toDouble()
                     }
