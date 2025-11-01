19efc172809a3be0ea7a60be7464d3edb77b5da1
That1Neet <nmarshall907@gmail.com>
2025-10-31 16:56:09 -0800

    s
    s

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.kt
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.subsystems
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+//import com.qualcomm.robotcore.hardware.external.Telemetry
+//import com.qualcomm.robotcore.hardware.robertMkII.hardware.NgMotor
+import java.util.Locale
 
-class Drivetrain {
-}
+
+
+class Drivetrain(hardwareMap: HardwareMap) {
+
+    private val frontLeft =
+    private val frontRight = (DcMotor)(hardwareMap.get("frontRight") as DcMotor, DcMotorSimple.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+    private val backLeft = (DcMotor)(hardwareMap.get("backLeft") as DcMotor, DcMotorSimple.Direction.REVERSE, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+    private val backRight = (DcMotor)(hardwareMap.get("backRight") as DcMotor, DcMotorSimple.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+
+    fun setSpeed(x: Double, y:Double, turn: Double) {
+        frontLeft.effort = y - x + turn
+        frontRight.effort = y + x - turn
+        backLeft.effort = y + x + turn
+        backRight.effort = y - x - turn
+
+    }
+
+    fun write() {
+        frontLeft.write()
+        frontRight.write()
+        backLeft.write()
+        backRight.write()
+    }
+
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ScMotor.kt
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import com.jakewharton.threetenabp.AndroidThreeTen.init
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
+import kotlin.math.abs
